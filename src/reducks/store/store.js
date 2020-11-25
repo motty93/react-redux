c803c26fa53e94a406b03d078c30ccc3325542e5
motty93 <rdwbocungelt5@gmail.com>
2020-11-25 14:43:16 +0900

    add: store add connectRouter & history

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,15 +1,21 @@
-import { createStore as reduxCreateStore, combineReducers } from 'redux'
-// import { ProductsReducer } from '../products/reducers'
+import {
+  createStore as reduxCreateStore,
+  combineReducers,
+  applyMiddleware
+} from 'redux'
+import { connectRouter, routerMiddleware } from 'connected-react-router'
 import { UsersReducer } from '../users/reducers'
 
 // combineReducers
 // 分割したReducresをまとめる・stateカテゴリ毎・オブジェクトをreturnする
 // initialStateと同じ形になるように作成していく
-export default function createStore() {
+export default function createStore(history) {
   return reduxCreateStore(
     combineReducers({
+      router: connectRouter(history),
       // products: ProductsReducer,
       users: UsersReducer
-    })
+    }),
+    applyMiddleware(routerMiddleware(history))
   )
 }
